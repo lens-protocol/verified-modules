1f66a991f31fc2026ca3a7f6831a6d75defe0135
Sasi <hey@sasi.codes>
2023-12-14 20:36:00 +0530

    feat: sync action (#2)

---
+++ scripts/sync.js
@@ -0,0 +1,122 @@
+const fs = require("fs");
+const axios = require("axios");
+
+const SECRET = process.env.SECRET ?? "";
+
+if (!SECRET.length) {
+  throw new Error("SECRET is required to run this script");
+}
+
+const API_URL = "https://api-v2.lens.dev";
+const mutation = `
+               mutation InternalUpdateModuleOptions($request: InternalUpdateModuleOptionsRequest!) {
+                  internalUpdateModuleOptions(request: $request)
+                }
+              `;
+
+const files = [
+  { filename: "follow-modules.json", type: "FOLLOW" },
+  { filename: "open-actions.json", type: "OPEN_ACTION" },
+  { filename: "reference-modules.json", type: "REFERENCE" },
+];
+
+const lastSyncedFileReadData = fs.readFileSync(
+  "scripts/last-synced.json",
+  "utf8"
+);
+const lastSyncedFileData = JSON.parse(lastSyncedFileReadData);
+
+let lastSynced = {};
+const verifyPromises = [];
+
+for (const file of files) {
+  const data = fs.readFileSync(file.filename, "utf8");
+  const modules = JSON.parse(data);
+
+  for (const module of modules) {
+    const moduleId = `${file.type}-${module.name}-${module.address}`;
+
+    const variables = {
+      request: {
+        i: module.address,
+        secret: SECRET,
+        t: file.type,
+        v: true,
+        lma: !module.requiresUserFunds,
+      },
+    };
+
+    const promise = axios
+      .post(API_URL, {
+        query: mutation,
+        variables,
+      })
+      .then((response) => {
+        console.log(
+          `Module Verified: ${module.name}, Response:`,
+          response.data
+        );
+        if (lastSynced[file.type]) {
+          if (lastSynced[file.type].includes(moduleId)) return;
+          lastSynced[file.type].push(moduleId);
+        } else {
+          lastSynced[file.type] = [moduleId];
+        }
+      })
+      .catch((error) => {
+        console.error(`Error verify module: ${module.name}`, error);
+      });
+
+    verifyPromises.push(promise);
+  }
+}
+
+Promise.all(verifyPromises).then(() => {
+  console.log("✅ All modules verified.");
+
+  console.log("⌛ Checking removed modules...");
+  const removedModules = {};
+  for (const key in lastSyncedFileData) {
+    removedModules[key] = lastSyncedFileData[key].filter(
+      (item) => !lastSynced[key]?.includes(item)
+    );
+  }
+
+  console.log("➡︎ Removed Modules -", removedModules);
+  for (const type in removedModules) {
+    for (const moduleKey of removedModules[type]) {
+      console.log("Unverifing module:", moduleKey);
+
+      const moduleType = moduleKey.split("-")[0];
+      const moduleAddress = moduleKey.split("-")[2];
+
+      axios
+        .post(API_URL, {
+          query: mutation,
+          variables: {
+            request: {
+              i: moduleAddress,
+              secret: SECRET,
+              t: moduleType,
+              v: false,
+              lma: false,
+            },
+          },
+        })
+        .then((response) => {
+          console.log(
+            `🚮 Module unverified: ${moduleKey}, Response:`,
+            response.data
+          );
+        })
+        .catch((error) => {
+          console.error(`Error unverify module: ${moduleKey}`, error);
+        });
+    }
+  }
+
+  fs.writeFileSync(
+    "scripts/last-synced.json",
+    JSON.stringify(lastSynced, null, 2)
+  );
+});
